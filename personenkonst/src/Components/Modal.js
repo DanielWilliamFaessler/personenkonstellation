4ad557fe15033cc4fd949d76adb860edd4745800
Jjaaminy <100757825+Jjaaminy@users.noreply.github.com>
2023-04-05 09:56:56 +0200

    updated and handleclose deleted

--- personenkonst/src/Components/Modal.js
+++ personenkonst/src/Components/Modal.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './Joined.css';
 import React, { useState, props, LoginForm } from 'react';
 
-function Modal({show, room, user}) {
+function Modal({show, room, user, author, book, handleclose }) {
+    console.log(show);
     if (!show) return null;
 
-    const handleclose =() => {
-        show(false);
-    }
+ 
 
     return (
         <div className={'modal'}>
             <div className={'modal-content'}>
-                <h2>Personnenkonstellation</h2>
-                <p>beigetreten als: {user} im Raum {room}</p>
-                <p>Autor: ________</p>
-                <p>Lektüre: ________</p>
+                <h2>Personenkonstellation</h2>
+                <p>beigetreten als: {user}</p>
+                <p>im Raum {room}</p>
+                <p>Autor: {author}</p>
+                <p>Lektüre: {book}</p>
                 <p>..Warte auf den Host um anzufangen..</p>
-                <button className={"close"} onClick={handleclose}>x</button>
+                <button className={"close"} onClick={handleclose} >x</button>
             </div>
         </div>
     )
