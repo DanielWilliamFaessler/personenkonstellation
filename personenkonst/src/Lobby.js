3fe43cde6a93a0bb5bd342cf0b08b0bd4a233d6f
Jjaaminy <100757825+Jjaaminy@users.noreply.github.com>
2023-04-05 09:55:24 +0200

    collaboration added

--- personenkonst/src/Lobby.js
+++ personenkonst/src/Lobby.js
@@ -1,36 +1,133 @@
 import logo from './logo.svg';
-import './Lobby.css';
-import React from 'react';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Joined from "./Joined";
+import Lobby from "./Lobby";
+import LobbyRandomPin from "./Lobby";
+import {Form} from "react-bootstrap";
+import useWebSocket from 'react-use-websocket';
 
-function LobbyRandomPin() {
-    const numbers = [];
-    for (let i = 0; i < 6; i++) {
-        const pin = Math.floor(Math.random() * 9) + 1;
-        numbers.push(pin);
+function App() {
+  const [showPopup, setShowPopup] = useState(false);
+  const [showLobby, setShowLobby] = useState(false);
+  const [autor, setAutor] = useState("");
+  const [lektüre, setLektüre] = useState("");
+
+  const [user, setUser] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [pin, setPin] = useState("");
+
+  const [error, setError] = useState("");
+
+  /*
+  //compare lobby pin and entered pin
+    const handleJoin = () => {
+        const ws = new WebSocket('ws://localhost:8000');
+
+        ws.onopen = () => {
+            console.log('Connected to server');
+            ws.send(`join:${pin}`);
+        };
+
+        ws.onmessage = (event) => {
+            console.log('Received message:', event.data);
+            setConnected(event.data);
+            ws.close();
+        };
+
+        ws.onclose = () => {
+            console.log('Disconnected from server');
+        };
+    };
+   */
+
+
+    const handleJoin = () => {
+      setShowPopup(true);
     }
-    return <div className={"numbers"}>{numbers.join('')}</div>;
-}
 
-function Lobby({show, author, book, code}) {
-    if (!show) return null;
+  const handleClosepop = () => {
+    setShowPopup(false);
+    setShowLobby(false);
+  };
 
-    return (
-        <div className={"lobby"}>
-            <div className="container">
-                <div className="left-half">
-                    <h1>Raum beitreten: </h1>
-                    <LobbyRandomPin value={code} />
-                    <h2>Personenkonstellation</h2>
-                    <p>Autor: {author}</p>
-                    <p>Lektüre: {book}</p>
+  const handleCloselob = () => {
+    setShowLobby(false);
+  };
+
+//create Lobby
+  const handleCreateLobby = () => {
+    setShowLobby(true);
+  };
+
+
+  //pin regex
+  const isValidPin = (code) => {
+    return /^\d{6}$/.test(code);
+  };
+
+
+
+
+  return (
+      <div id={"back"}>
+
+        {/*call components*/}
+        <section>
+          <h1 className={'title'}>Personenkonstellation</h1>
+        </section>
+        <Joined  show={showPopup} room={pin} user={user}  book={lektüre} author={autor} handleclose={handleClosepop} />
+        {/*} <Lobby show={showLobby} book={lektüre} author={autor}   joiner={user} handleclose={handleCloselob}/>*/}
+        <Lobby  show={showLobby} book={lektüre} author={autor}   joiner={user}  room={pin} handleclose={handleCloselob}/>
+
+        <div className={'container'}>
+          {/*create room field*/}
+          <div className={'box1'}>
+            <div id={"erstellen"}>
+              <h3 >Raum erstellen:</h3>
+              <label>Lektüretitel eingeben: </label>
+              <br/>
+              <input type={'text'} onChange={e => setLektüre(e.target.value)} />
+              <br/>
+              <br/>
+              <label>Autorenname eingeben: </label>
+              <br/>
+              <input type={'text'}  onChange={e => setAutor(e.target.value)}/>
+              <br/>
+              <br/>
+              <button   onClick={handleCreateLobby}>erstellen</button>
             </div>
-            <div className="right-half">
-                <h3 className={"joiningUsers"}>Personen beigetreten: </h3>
-                <a>show users which joined</a>
+          </div>
+
+          {/*join room field*/}
+          <div className={'box1'}>
+            {connected ? (
+                <Lobby />
+            ) : (
+            <div  id={"beitreten"}>
+              <h3>Raum beitreten:</h3>
+              <label>Benutzername eingeben: </label>
+              <br/>
+              <input type={'text'} value={user} onChange={(e) => setUser(e.target.value)}/>
+              <br/>
+              <br/>
+              <label>Raumcode eingeben: </label>
+              <br/>
+              <input type={'text'} value={pin} onChange={(e) => setPin(e.target.value)} />
+              <br/>
+              <br/>
+              <button onClick={handleJoin}>beitreten</button>
+                {/*
+                {connected === 'success' && <p>Successfully joined lobby!</p>}
+                {connected === 'error' && <p>Invalid lobby pin.</p>}
+                */}
+
             </div>
+                )}
         </div>
         </div>
-    )
+        </div>
+  );
 }
 
-export default Lobby;
+export default App;
