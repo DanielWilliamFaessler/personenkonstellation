19fc52b8413e266f4c4c7cb710c6b10f793a463b
Jjaaminy <100757825+Jjaaminy@users.noreply.github.com>
2023-04-20 13:25:39 +0200

    updated socket

--- personenkonst/src/Components/Modal.js
+++ personenkonst/src/Components/Modal.js
@@ -1,26 +1,40 @@
-import logo from './logo.svg';
 import './Joined.css';
-import React, { useState, props, LoginForm } from 'react';
+import React, { useState, useEffect, LoginForm, handleclose } from 'react';
+import Tool from './Tool';
+import ColoredRect from './App';
 
-function Modal({show, room, user, author, book, handleclose }) {
+function Modal({show, user,  handleclose}) {
+    const [message, setMessage] = useState('');
+    const [tool, setTool] = useState(false);
+
+    const handleClick = () => {
+        setTool(true);
+    };
+
     console.log(show);
     if (!show) return null;
 
- 
-
     return (
-        <div className={'modal'}>
-            <div className={'modal-content'}>
-                <h2>Personenkonstellation</h2>
-                <p>beigetreten als: {user}</p>
-                <p>im Raum {room}</p>
-                <p>Autor: {author}</p>
-                <p>Lektüre: {book}</p>
-                <p>..Warte auf den Host um anzufangen..</p>
-                <button className={"close"} onClick={handleclose} >x</button>
+        <div>
+            <div className={'modal'}>
+                {tool ? (
+                    <ColoredRect />
+                ) : (
+                    <div className={'modal-content'}>
+
+                        <h2>Personenkonstellation</h2>
+                        <div className="joined">
+                            <p>name: {user}</p>
+                            {/*  <button onClick={handleClick}>go</button> */}
+                            <p>{message}</p>
+                        </div>
+                        <p>..Warte auf den Host um anzufangen..</p>
+                        <button className={"close"} onClick={handleclose} >x</button>
+                    </div>
+                )}
             </div>
         </div>
     )
 }
 
-export default Modal;
+export default Joined;
