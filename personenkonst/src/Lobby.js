49d3220234cf45ceb1732137ba91962d392294e3
Jjaaminy <100757825+Jjaaminy@users.noreply.github.com>
2023-04-20 13:23:28 +0200

    updated socket

--- personenkonst/src/Lobby.js
+++ personenkonst/src/Lobby.js
@@ -1,80 +1,80 @@
 import './Lobby.css';
 import React, { useState, useEffect } from 'react';
-const WebSocket = require('ws');
+import Tool from './Tool';
+import ColoredRect from './App';
+import io from "socket.io-client";
 
-function Lobby({pop,show, author, book, code,joiner,handleclose}) {
-    const [users,setUsers] = useState([]);
-    console.log(show);
+
+const socket = io('http://localhost:5000');
+
+
+function Lobby({room, pop,show, author, book,handleclose}) {
+    const [clients, setClients] = useState([]);
+    const [tool, setTool] = useState(false);
+    const [pin, setPin] = useState(false);
+
+    useEffect(() => {
+        socket.on('lobby_created', ({ pin }) => {
+            setPin(pin);
+        });
+
+        //get clients
+        socket.on('room_joined', ({ clients }) => {
+            setClients(clients);
+        });
+
+        // Clean up the socket listener when the component unmounts
+        return () => {
+            socket.off('lobby_created');
+        };
+
+    }, []);
+
+    const handleStartRoom = () => {
+       socket.emit('start_room', { pin });
+        setTool(true);
+    };
+
+    // Listen for "room_started" event
+    socket.on('room_started', (pin) => {
+        // Redirect all users in the room to the tool component
+        window.location.href = `/App.js?pin=${pin}`;
+    });
+
     if (!show) return null;
 
-
-  /*  const socket = new WebSocket('ws://localhost:8080');
-
-    socket.addEventListener('open', (event) => {
-        // Handle connection opening
-    });
-
-    socket.addEventListener('message', (event) => {
-        const data = JSON.parse(event.data);
-
-        if (data.type === 'user-joined') {
-            // Handle new user joining the lobby
-            const user = data.user;
-            users[user.id] = user;
-            updateUserList();
-        } else if (data.type === 'user-left') {
-            // Handle user leaving the lobby
-            const userId = data.userId;
-            delete users[userId];
-            updateUserList();
-        }
-    });
-
-    socket.addEventListener('close', (event) => {
-        console.log('Disconnected from server');
-        // Update UI to indicate that the connection has been closed
-    });
-
-    function updateUserList() {
-        const userList = document.querySelector('#user-list');
-        userList.innerHTML = '';
-
-        Object.values(users).forEach((user) => {
-            const li = document.createElement('li');
-            li.innerText = user.name;
-            userList.appendChild(li);
-        });
-    }
-
-   */
-
-
-// Generate random 6 digit pin for lobby
-    let lobbyPin = Math.floor(100000 + Math.random() * 900000).toString();
-    console.log('Lobby pin:', lobbyPin);
-
     return (
-        <div className={"lobby"}>
-            <div className="container">
-                <div className="left-half">
-                    <h1>Raum beitreten: </h1>
-                    <fieldset className={'numbers'} >{lobbyPin}</fieldset>
-                    <h2>Personenkonstellation</h2>
-                    <p>Autor: {author}</p>
-                    <p>Lektüre: {book}</p>
-
+        <div className="lobby">
+            {tool ? (
+                <ColoredRect pin={pin} />
+            ) : (
+                <div className="container">
+                    <div className="left-half">
+                        <h1>Raum beitreten: </h1>
+                        <fieldset>{pin}</fieldset>
+                        <h2>Personenkonstellation</h2>
+                        <p>Autor: {author}</p>
+                        <p>Lektüre: {book}</p>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <button  className={"start"} onClick={handleStartRoom}>Start-tool</button>
+                    </div>
+                    <div className="right-half">
+                        <h3 className="joiningUsers">Personen beigetreten:</h3>
+                        <a>beigetretene Benutzer: </a>
+                        {clients.map((client) => (
+                            <li key={client}>{client}</li>
+                        ))}
+                        <button className="close" onClick={handleclose}>
+                            x
+                        </button>
+                    </div>
                 </div>
-                <div className="right-half" >
-                    <h3 className="joiningUsers" >  Personen beigetreten: </h3>
-                    <a>beigetretene Benutzer: </a>
-                    <ul>
-                        {joiner}
-                    </ul>
-                    <button className={"close"} onClick={handleclose} >x</button>
-                </div>
-            </div>
+            )}
         </div>
     )
 }
 
 export default Lobby;
+
